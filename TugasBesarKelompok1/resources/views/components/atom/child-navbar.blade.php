29e9166fd22f5f87f57c58d688e3cb201f738b57
GenisGenerik <164584630+GenisGenerik@users.noreply.github.com>
2024-12-07 17:41:55 +0700

    permulaan

--- TugasBesarKelompok1/resources/views/components/atom/child-navbar.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="{{ $styles }}">
-    <a href="">{{ $slot }}</a>
-</div>
--- TugasBesarKelompok1/resources/views/components/atom/logo.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="flex sm:flex-row justify-between ml-4 sm:ml-0">
-    <img src="{{ asset('gambar/logo.png') }}" alt=""
-      class="w-20 sm:w-32 h-10 ml-2 sm:ml-20 sm:my-2 pt-2 sm:pt-0 pb-1 sm:pb-0">
-  </div>
